e8dbddfe23d46939d0ff938418d36f88bc63b3b8
David Morrow <quickbasic44@outlook.com>
2021-09-19 19:44:33 -0400

    Add project files.

---
+++ BicycleVectors/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace BicycleVectors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBikeSpeed = new System.Windows.Forms.TextBox();
+            this.txtBikeDirection = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtWindSpeed = new System.Windows.Forms.TextBox();
+            this.txtWindDirection = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBikeSpeed
+            // 
+            this.txtBikeSpeed.Location = new System.Drawing.Point(124, 154);
+            this.txtBikeSpeed.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtBikeSpeed.Name = "txtBikeSpeed";
+            this.txtBikeSpeed.Size = new System.Drawing.Size(260, 38);
+            this.txtBikeSpeed.TabIndex = 0;
+            // 
+            // txtBikeDirection
+            // 
+            this.txtBikeDirection.Location = new System.Drawing.Point(508, 154);
+            this.txtBikeDirection.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtBikeDirection.Name = "txtBikeDirection";
+            this.txtBikeDirection.Size = new System.Drawing.Size(260, 38);
+            this.txtBikeDirection.TabIndex = 1;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(157, 715);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(509, 105);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "CalculateVectorResults";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 31);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Bike Speed";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(546, 74);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 31);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Bike Direction";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 319);
+            this.label4.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Wind Speed";
+            // 
+            // txtWindSpeed
+            // 
+            this.txtWindSpeed.Location = new System.Drawing.Point(124, 424);
+            this.txtWindSpeed.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtWindSpeed.Name = "txtWindSpeed";
+            this.txtWindSpeed.Size = new System.Drawing.Size(260, 38);
+            this.txtWindSpeed.TabIndex = 13;
+            // 
+            // txtWindDirection
+            // 
+            this.txtWindDirection.Location = new System.Drawing.Point(508, 424);
+            this.txtWindDirection.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.txtWindDirection.Name = "txtWindDirection";
+            this.txtWindDirection.Size = new System.Drawing.Size(260, 38);
+            this.txtWindDirection.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(546, 331);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 31);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Wind Direction";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(603, 1075);
+            this.label5.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 31);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Indigo;
+            this.label6.Location = new System.Drawing.Point(266, 519);
+            this.label6.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label6.MaximumSize = new System.Drawing.Size(1600, 1192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(400, 230);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(1604, 881);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWindDirection);
+            this.Controls.Add(this.txtWindSpeed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtBikeDirection);
+            this.Controls.Add(this.txtBikeSpeed);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBikeSpeed;
+        private System.Windows.Forms.TextBox txtBikeDirection;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtWindSpeed;
+        private System.Windows.Forms.TextBox txtWindDirection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ BicycleVectors/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BicycleVectors
+{
+    public partial class Form1 : Form
+    {
+        public double BikeSpeed;
+        public double BikeDirection;
+        public double WindSpeed;
+        public double WindDirection;
+        PointF[] graphicPoint;
+        double BikeXDir;
+        double BikeYDir;
+        double WindXDir;
+        double WindYDir;
+        Rectangle formBounds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphicPoint = new PointF[1] { new PointF(0f, 0f) };
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        { 
+            BikeSpeed = double.Parse(txtBikeSpeed.Text);
+            BikeDirection = double.Parse(txtBikeDirection.Text);
+            WindSpeed = double.Parse(txtWindSpeed.Text);
+            WindDirection = double.Parse(txtWindDirection.Text);
+            BikeDirection -= 90;
+            WindDirection -= 90;
+            BikeXDir = ((BikeSpeed * 0.3) * Math.Cos(ConvertDegreesToRadians(BikeDirection)));
+            BikeYDir = ((BikeSpeed * 0.3) * Math.Sin(ConvertDegreesToRadians(BikeDirection)));
+            WindXDir = ((WindSpeed * 0.5) * Math.Cos(ConvertDegreesToRadians(WindDirection)));
+            WindYDir = ((WindSpeed * 0.5) * Math.Sin(ConvertDegreesToRadians(WindDirection)));
+            BikeXDir = ElimateExtremeValues(BikeXDir);
+            BikeYDir = ElimateExtremeValues(BikeYDir);
+            WindXDir = ElimateExtremeValues(WindXDir);
+            WindYDir = ElimateExtremeValues(WindYDir);
+
+
+
+            (double BikeR, double BikeTheta) = ConvertPolarToRectangular(BikeSpeed, BikeDirection);
+            (double WindR, double WindTheta) = ConvertPolarToRectangular(WindSpeed, WindDirection);
+            double Speed = BikeR + WindR;
+            double Direction = BikeTheta + WindTheta;             
+            (BikeSpeed, BikeDirection) = ConvertRectangularToPolar(Speed, Direction);
+            label6.Text = "";
+            label6.Text += string.Format($"bike X {BikeXDir} bike Y  {BikeYDir} \r\n");
+            label6.Text += string.Format($"wind X {WindXDir} wind Y {WindYDir} \r\n");
+            this.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtBikeSpeed.Text = "500";
+            txtBikeDirection.Text = "0";
+            txtWindSpeed.Text = "100";
+            txtWindDirection.Text = "90";
+            BikeXDir = ((BikeSpeed * 0.3) * Math.Cos(BikeDirection));
+            BikeYDir = ((BikeSpeed * 0.3) * Math.Sin(BikeDirection));
+            WindXDir = ((WindSpeed * 0.5) * Math.Cos(WindDirection));
+            WindYDir = ((WindSpeed * 0.5) * Math.Sin(WindDirection));
+            label6.Text = "";
+            label6.Text = string.Format($"bike X {BikeXDir} bike Y {BikeYDir} \r\n");
+            label6.Text += string.Format($"wind X {WindXDir} wind Y {WindYDir} \r\n");
+            this.Refresh();
+          
+            formBounds = this.Bounds;
+            formBounds.Offset(x: 100, y: 50);
+
+
+
+
+        }
+
+        public (double R, double Theta) ConvertRectangularToPolar(double x, double y)
+        {          
+            double R = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            double Theta = Math.Atan2(y, x);
+            double ThetaDegrees = ConvertRadiansToDegrees(Theta);
+            
+            if (ThetaDegrees <= -1)
+                ThetaDegrees = 360 - ThetaDegrees;
+            return (R, ThetaDegrees);
+        }
+
+        public (double x, double y) ConvertPolarToRectangular(double r, double Theta)
+        {
+            double RadianTheta = ConvertDegreesToRadians(Theta);         
+            double x = r * Math.Cos(RadianTheta);
+            double y = r * Math.Sin(RadianTheta);
+            
+            return (x, y);
+        }
+
+        public double AdjustAngleFromQuadrant(double result, double xVar, double yVar)
+        {
+            if (xVar < 0 && yVar < 0)
+            {
+                result = (180 / Math.PI) * result;
+                result = result + 180;
+            }
+            else if (yVar < 0)
+            {
+                result = (180 / Math.PI) * result;
+                result = 360 - result;
+            }
+            else if (xVar < 0 && yVar == 0)
+            {
+                result = 180;
+            }
+            else
+            {
+                result = (180 / Math.PI) * result;
+            }
+            return result;
+        }
+
+        public double ConvertDegreesToRadians(double val)
+        {
+            val = (Math.PI / 180 * val);
+            return val;
+        }
+
+        public double ConvertRadiansToDegrees(double val)
+        {
+            return (180 / Math.PI * val);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            graphicPoint[0].X = e.X;
+            graphicPoint[0].Y = e.Y;
+            this.Refresh();
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            //e.Graphics.PageUnit = GraphicsUnit.Pixel;
+            //e.Graphics.ScaleTransform(1.36f, -1.27f);
+            //e.Graphics.TranslateTransform(800f, 419f);
+
+            e.Graphics.PageUnit = GraphicsUnit.Pixel;
+            e.Graphics.ScaleTransform(3.2f, -1.674f);
+            e.Graphics.TranslateTransform(350f, -250f);
+
+            if (graphicPoint != null) ;
+            e.Graphics.TransformPoints(System.Drawing.Drawing2D.CoordinateSpace.World, System.Drawing.Drawing2D.CoordinateSpace.Page, graphicPoint);
+            label5.Text = graphicPoint[0].X + " " + graphicPoint[0].Y;
+            float bikex = float.Parse(BikeXDir.ToString());
+            float bikey = float.Parse(BikeYDir.ToString());
+            float windx = float.Parse(WindXDir.ToString());
+            float windy = float.Parse(WindYDir.ToString());
+           
+            
+            PointF BikeStart = new PointF(0F, 0f);
+            PointF BikeEnd = new PointF(bikex, bikey);
+            PointF WindStart = new PointF(0f, 0f);
+            PointF WindEnd = new PointF(windx, windy);
+            Pen BluePen = new Pen(Color.Blue, 10f);
+            Pen RedPen = new Pen(Color.Red, 10f);
+            //BikeEnd.Y = -BikeEnd.Y;
+            //WindEnd.Y = -WindEnd.Y;
+           
+            BluePen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            RedPen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            e.Graphics.DrawLine(BluePen, BikeStart, BikeEnd);
+            e.Graphics.DrawLine(RedPen, WindStart, WindEnd);
+            e.Graphics.DrawLine(BluePen, BikeStart, BikeEnd);
+            e.Graphics.DrawLine(RedPen, WindStart, WindEnd);
+            PointF Point = new PointF(0f, 0f);
+            SizeF Size = new SizeF(10f, 10f);
+            RectangleF arcRec = new RectangleF(Point, Size);
+            BluePen.Color = Color.PeachPuff;
+            e.Graphics.DrawArc(BluePen, arcRec, 0, 360);
+        }
+
+        private double ElimateExtremeValues(double value)
+        {
+            double intPart = Math.Truncate(value);
+            double fracPart = value - intPart;
+            if (fracPart < 0.01)
+            {
+                fracPart = 0;
+               value = intPart + fracPart;
+            }
+
+            return value;
+        }
+
+    }
+}
